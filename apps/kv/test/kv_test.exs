a024ccc59818f376ea0c045ad99add2581185b29
Joe-noh <goflb.jh@gmail.com>
2016-08-17 03:00:46 +0900

    add some test

--- apps/kv/test/kv_test.exs
+++ apps/kv/test/kv_test.exs
@@ -2,7 +2,16 @@ defmodule KvTest do
   use ExUnit.Case
   doctest Kv
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "put and get works" do
+    Kv.Store.put(:a, 1)
+
+    assert Kv.Store.get(:a) == 1
+  end
+
+  test "overwrite" do
+    Kv.Store.put(:a, 1)
+    Kv.Store.put(:a, 2)
+
+    assert Kv.Store.get(:a) == 2
   end
 end
