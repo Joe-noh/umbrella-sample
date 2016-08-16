6bcb547761df57fde8b2f97ada728cad5e4b0a8e
Joe-noh <goflb.jh@gmail.com>
2016-08-17 03:20:18 +0900

    mix new basket --sup

---
+++ apps/basket/lib/basket.ex
@@ -0,0 +1,20 @@
+defmodule Basket do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Basket.Worker.start_link(arg1, arg2, arg3)
+      # worker(Basket.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Basket.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ apps/basket/test/basket_test.exs
@@ -0,0 +1,8 @@
+defmodule BasketTest do
+  use ExUnit.Case
+  doctest Basket
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
