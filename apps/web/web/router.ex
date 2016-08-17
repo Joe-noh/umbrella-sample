b8ec9a3ee96ab877bff2ce83ccf50ea835d98e3b
Joe-noh <goflb.jh@gmail.com>
2016-08-17 17:30:47 +0900

    define routes

--- apps/web/web/router.ex
+++ apps/web/web/router.ex
@@ -16,7 +16,11 @@ defmodule Web.Router do
   scope "/", Web do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get  "/basket/:basket_id", BasketController, :show
+    post "/basket",            BasketController, :create
+
+    post "/basket/:basket_id/item", BasketItemController, :create
+    post "/basket/:basket_id/checkout", CheckoutController, :create
   end
 
   # Other scopes may use custom stacks.
